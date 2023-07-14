a7734e3e68d091582fbb5f571493d81aaa17d5b8
rohulaminwd <rohulaminwd@gmail.com>
2023-07-14 23:31:44 +0600

    setup all  files

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { UserRoutes } from '../modules/user/user.route';
+import { BookRoutes } from '../modules/books/book.route';
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/book',
+    route: BookRoutes,
   },
 ];
 
